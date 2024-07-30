be71fa7fb03f19225aec443514527cf6683c410b
tanmoy <tanmoy22chowdhury@gmail.com>
2024-07-30 19:05:53 +0530

    type declared for cofig

--- test/configs/environments.ts
+++ test/configs/environments.ts
@@ -1,4 +1,7 @@
-export const environments = {
+type indexableType = {
+    [key: string]: any
+};
+export const environments:indexableType = {
     qa: {
         baseurl: 'https://services.petsmart.com'
     }
